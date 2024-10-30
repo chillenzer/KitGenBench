321421a472748b421c1852a22007a9c8e22a2f27
Julian Lenz <j.lenz@hzdr.de>
2024-10-30 16:00:57 +0100

    Parse commandline options

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -1,51 +1,49 @@
 #include <membenchmc/membenchmc.h>
 #include <membenchmc/version.h>
 
+#include <cstdlib>
 #include <cxxopts.hpp>
 
-auto main(/*int argc, char** argv*/) -> int {
-  std::cout << membenchmc::gatherMetadata() << "\n";
-  //  const std::unordered_map<std::string, membenchmc::LanguageCode> languages{
-  //      {"en", membenchmc::LanguageCode::EN},
-  //      {"de", membenchmc::LanguageCode::DE},
-  //      {"es", membenchmc::LanguageCode::ES},
-  //      {"fr", membenchmc::LanguageCode::FR},
-  //  };
-  //
-  //  cxxopts::Options options(*argv, "A program to welcome the world!");
-  //
-  //  std::string language;
-  //  std::string name;
-  //
-  //  // clang-format off
-  //  options.add_options()
-  //    ("h,help", "Show help")
-  //    ("v,version", "Print the current version number")
-  //    ("n,name", "Name to greet", cxxopts::value(name)->default_value("World"))
-  //    ("l,lang", "Language code to use", cxxopts::value(language)->default_value("en"))
-  //  ;
-  //  // clang-format on
-  //
-  //  auto result = options.parse(argc, argv);
-  //
-  //  if (result["help"].as<bool>()) {
-  //    std::cout << options.help() << std::endl;
-  //    return 0;
-  //  }
-  //
-  //  if (result["version"].as<bool>()) {
-  //    std::cout << "MemBenchMC, version " << MEMBENCHMC_VERSION << std::endl;
-  //    return 0;
-  //  }
-  //
-  //  auto langIt = languages.find(language);
-  //  if (langIt == languages.end()) {
-  //    std::cerr << "unknown language code: " << language << std::endl;
-  //    return 1;
-  //  }
-  //
-  //  membenchmc::MemBenchMC membenchmc(name);
-  //  std::cout << membenchmc.greet(langIt->second) << std::endl;
+auto defineCmdlineInterface() {
+  cxxopts::Options options("my_program", "A program to process a configuration file");
+  // clang-format off
+  options.add_options()
+    ("c,config", "Path to the configuration file", cxxopts::value<std::string>())
+    ("h,help", "Print help")
+    ( "v,version", "Print the current version number");
+  // clang-format on
+  return options;
+}
 
-  return 0;
+auto extractConfigFilePath(int argc, char* argv[], auto& options) {
+  auto result = options.parse(argc, argv);
+  // Check if help was requested
+  if (result.count("help")) {
+    std::cout << options.help() << std::endl;
+    exit(0);
+  }
+  if (result["version"].template as<bool>()) {
+    std::cout << "MemBenchMC, version " << MEMBENCHMC_VERSION << std::endl;
+    exit(0);
+  }
+  // Check if the config option was provided
+  if (!result.count("config")) {
+    std::cerr << "Error: No configuration file specified. Use --config <path_to_config_file>."
+              << std::endl;
+    exit(1);
+  }
+  // Get the path to the configuration file
+  return result["config"].template as<std::string>();
+}
+
+auto main(int argc, char* argv[]) -> int {
+  auto metadata = membenchmc::gatherMetadata();
+  auto options = defineCmdlineInterface();
+  try {
+    auto configFilePath = extractConfigFilePath(argc, argv, options);
+  } catch (const cxxopts::OptionException& e) {
+    std::cerr << "Error parsing options: " << e.what() << std::endl;
+    exit(1);
+  }
+  return EXIT_SUCCESS;
 }
