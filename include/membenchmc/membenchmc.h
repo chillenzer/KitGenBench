2d8e66e6cd72ed15389bef85b5a62a5ef57ee4d8
Julian Lenz <j.lenz@hzdr.de>
2024-11-01 00:03:29 +0100

    Add basic workflow

--- include/membenchmc/membenchmc.h
+++ include/membenchmc/membenchmc.h
@@ -1,6 +1,9 @@
 #pragma once
 #include <alpaka/alpaka.hpp>
 #include <nlohmann/json.hpp>
+#include <optional>
+
+#include "alpaka/queue/Properties.hpp"
 
 namespace membenchmc {
 
@@ -8,9 +11,53 @@ namespace membenchmc {
     return std::make_tuple(1, 2, 3);
   }
 
-  template <typename TAcc> nlohmann::json runBenchmark([[maybe_unused]] auto& recipes,
-                                                       [[maybe_unused]] auto& loggers,
-                                                       [[maybe_unused]] auto& checkers) {
+  struct StepResult {};
+
+  template <typename TRecipe> std::optional<StepResult> next([[maybe_unused]] TRecipe& recipe) {
+    return {};
+  }
+  template <typename TChecker> void check([[maybe_unused]] TChecker& checker,
+                                          [[maybe_unused]] std::optional<StepResult> result) {}
+
+  template <typename TLogger>
+  std::optional<StepResult> callLogged([[maybe_unused]] TLogger& logger,
+                                       [[maybe_unused]] auto const& func) {
+    return {};
+  }
+
+  struct BenchmarkKernel {
+    template <typename TAcc>
+    ALPAKA_FN_ACC auto operator()(TAcc const& acc, auto recipes, auto loggers, auto checkers) const
+        -> void {
+      auto const globalThreadIdx = alpaka::getIdx<alpaka::Grid, alpaka::Threads>(acc);
+      auto const globalThreadExtent = alpaka::getWorkDiv<alpaka::Grid, alpaka::Threads>(acc);
+      auto const linearizedGlobalThreadIdx
+          = alpaka::mapIdx<1u>(globalThreadIdx, globalThreadExtent).x();
+
+      auto& myRecipe = recipes[linearizedGlobalThreadIdx];
+      auto& myLogger = loggers[linearizedGlobalThreadIdx];
+      auto& myChecker = checkers[linearizedGlobalThreadIdx];
+
+      std::optional<StepResult> result{};
+      do {
+        result = callLogged(myLogger, [&myRecipe] { return next(myRecipe); });
+        callLogged(myLogger, [&myChecker, &result] { return check(myChecker, result); });
+      } while (result);
+    }
+  };
+
+  template <typename TAcc>
+  nlohmann::json runBenchmark(auto const& workdiv, [[maybe_unused]] auto& recipes,
+                              [[maybe_unused]] auto& loggers, [[maybe_unused]] auto& checkers) {
+    auto const platformAcc = alpaka::Platform<TAcc>{};
+    auto const dev = alpaka::getDevByIdx(platformAcc, 0);
+    auto queue = alpaka::Queue<TAcc, alpaka::Blocking>(dev);
+
+    // Here it is synchronous which means that the kernel is directly executed.
+    alpaka::exec<TAcc>(queue, workdiv, BenchmarkKernel{}, recipes.template get<TAcc>(),
+                       loggers.template get<TAcc>(), checkers.template get<TAcc>());
+    alpaka::wait(queue);
+
     return {{alpaka::getAccName<TAcc>(), ""}};
   };
 
--- test/source/membenchmc.cpp
+++ test/source/membenchmc.cpp
@@ -7,28 +7,37 @@
 
 #include "alpaka/acc/AccCpuSerial.hpp"
 #include "alpaka/acc/Traits.hpp"
+#include "alpaka/workdiv/WorkDivHelpers.hpp"
+#include "alpaka/workdiv/WorkDivMembers.hpp"
 #include "nlohmann/json.hpp"
 
 struct NoRecipe {};
 struct NoChecker {};
 struct NoLogger {};
 
+template <typename TRecipe> struct Aggregate {
+  TRecipe recipe;
+  template <typename TAcc> auto get() { return std::span<TRecipe, 1>(&recipe, 1); }
+};
+
 using Dim = alpaka::DimInt<1>;
 using Idx = std::uint32_t;
-using TAcc = alpaka::AccCpuSerial<Dim, Idx>;
+using Acc = alpaka::AccCpuSerial<Dim, Idx>;
 
 TEST_CASE("runBenchmark trivially") {
   using namespace membenchmc;
 
-  auto recipes = NoRecipe{};
-  auto loggers = NoLogger{};
-  auto checkers = NoChecker{};
+  auto workdiv = alpaka::WorkDivMembers<Dim, Idx>{
+      alpaka::Vec<Dim, Idx>{1}, alpaka::Vec<Dim, Idx>{1}, alpaka::Vec<Dim, Idx>{1}};
+  auto recipes = Aggregate<NoRecipe>{};
+  auto loggers = Aggregate<NoLogger>{};
+  auto checkers = Aggregate<NoChecker>{};
 
-  SUBCASE("can do nothing.") { runBenchmark<TAcc>(recipes, loggers, checkers); }
+  SUBCASE("can do nothing.") { runBenchmark<Acc>(workdiv, recipes, loggers, checkers); }
 
   SUBCASE("returns a report about that accelerator.") {
-    auto report = runBenchmark<TAcc>(recipes, loggers, checkers);
-    CHECK(report.contains(alpaka::getAccName<TAcc>()));
+    auto report = runBenchmark<Acc>(workdiv, recipes, loggers, checkers);
+    CHECK(report.contains(alpaka::getAccName<Acc>()));
   }
 }
 
