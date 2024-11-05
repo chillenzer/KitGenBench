1451bf7f2fa8561407f5eb12244be31140f90e87
Julian Lenz <j.lenz@hzdr.de>
2024-11-05 14:18:48 +0100

    Clean up before next steps

--- include/membenchmc/DeviceClock.h
+++ include/membenchmc/DeviceClock.h
@@ -8,32 +8,34 @@
 #  include <cuda_runtime.h>
 #endif  //  alpaka_ACC_GPU_CUDA_ENABLE
 
-template <typename TAccTag> struct DeviceClock;
+namespace membenchmc {
+  template <typename TAccTag> struct DeviceClock;
 
-template <> struct DeviceClock<alpaka::TagCpuSerial> {
-  using DurationType = float;
-  ALPAKA_FN_INLINE ALPAKA_FN_ACC static auto clock() {
-    return std::chrono::high_resolution_clock::now();
-  }
+  template <> struct DeviceClock<alpaka::TagCpuSerial> {
+    using DurationType = float;
+    ALPAKA_FN_INLINE ALPAKA_FN_ACC static auto clock() {
+      return std::chrono::high_resolution_clock::now();
+    }
 
-  ALPAKA_FN_INLINE ALPAKA_FN_ACC static auto duration(auto start, auto end) {
-    // returning milliseconds
-    return static_cast<float>(
-               std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count())
-           / 1000000;
-  }
-};
+    ALPAKA_FN_INLINE ALPAKA_FN_ACC static auto duration(auto start, auto end) {
+      // returning milliseconds
+      return static_cast<float>(
+                 std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count())
+             / 1000000;
+    }
+  };
 
 #ifdef ALPAKA_ACC_GPU_CUDA_ENABLED
 
-template <> struct DeviceClock<alpaka::TagGpuCudaRt> {
-  using DurationType = float;
-  ALPAKA_FN_INLINE __device__ static auto clock() { return clock64(); }
+  template <> struct DeviceClock<alpaka::TagGpuCudaRt> {
+    using DurationType = float;
+    ALPAKA_FN_INLINE __device__ static auto clock() { return clock64(); }
 
-  ALPAKA_FN_INLINE ALPAKA_FN_ACC static auto duration(auto start, auto end) {
-    return start <= end ? end - start
-                        : std::numeric_limits<decltype(clock64())>::max() - start + end;
-  }
-};
+    ALPAKA_FN_INLINE ALPAKA_FN_ACC static auto duration(auto start, auto end) {
+      return start <= end ? end - start
+                          : std::numeric_limits<decltype(clock64())>::max() - start + end;
+    }
+  };
 
 #endif
+}  // namespace membenchmc
--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -18,64 +18,7 @@ namespace membenchmc::Actions {
   [[maybe_unused]] static constexpr int FREE = 2;
 }  // namespace membenchmc::Actions
 
-using Dim = alpaka::DimInt<1>;
-using Idx = std::uint32_t;
-using Acc = alpaka::TagToAcc<std::remove_cvref_t<decltype(std::get<0>(alpaka::EnabledAccTags{}))>,
-                             Dim, Idx>;
-
 namespace setups {
-
-  auto makeExecutionDetails() {
-    auto const platformAcc = alpaka::Platform<Acc>{};
-    auto const dev = alpaka::getDevByIdx(platformAcc, 0);
-    auto workdiv = alpaka::WorkDivMembers<Dim, Idx>{
-        alpaka::Vec<Dim, Idx>{1}, alpaka::Vec<Dim, Idx>{1}, alpaka::Vec<Dim, Idx>{1}};
-    return ExecutionDetails<Acc, decltype(dev)>{workdiv, dev};
-  }
-
-  template <typename TRecipe> struct Aggregate {
-    using type = TRecipe;
-
-    TRecipe recipe;
-
-    static constexpr size_t size() { return 1U; }
-
-    auto get([[maybe_unused]] auto& queue, [[maybe_unused]] auto const& device) {
-      return std::span<TRecipe, 1>(&recipe, 1);
-    }
-
-    auto generateReport() { return recipe.generateReport(); }
-  };
-
-  template <typename TAccTag> struct DeviceClock;
-
-  template <> struct DeviceClock<alpaka::TagCpuSerial> {
-    using DurationType = float;
-    ALPAKA_FN_INLINE ALPAKA_FN_ACC static auto clock() {
-      return std::chrono::high_resolution_clock::now();
-    }
-
-    ALPAKA_FN_INLINE ALPAKA_FN_ACC static auto duration(auto start, auto end) {
-      // returning milliseconds
-      return static_cast<float>(
-                 std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count())
-             / 1000000;
-    }
-  };
-
-#ifdef ALPAKA_ACC_GPU_CUDA_ENABLED
-
-  template <> struct DeviceClock<alpaka::TagGpuCudaRt> {
-    using DurationType = float;
-    ALPAKA_FN_INLINE __device__ static auto clock() { return clock64(); }
-
-    ALPAKA_FN_INLINE ALPAKA_FN_ACC static auto duration(auto start, auto end) {
-      return start <= end ? end - start
-                          : std::numeric_limits<decltype(clock64())>::max() - start + end;
-    }
-  };
-
-#endif
 
   template <typename TAccTag> struct SimpleSumLogger {
     DeviceClock<TAccTag>::DurationType mallocDuration;
@@ -113,35 +56,6 @@ namespace setups {
           {"deallocation average time [ms]", freeDuration / (freeCounter > 0 ? freeCounter : 1U)},
           {"deallocation count ", freeCounter},
       };
-    }
-  };
-
-  template <typename T> struct DeviceAggregate {
-    std::span<typename T::type, T::size()> instances{};
-
-    ALPAKA_FN_ACC T::type load([[maybe_unused]] auto const index) { return {}; }
-
-    ALPAKA_FN_ACC void store(T::type&& instance, auto const index) {
-      instances[index] = std::move(instance);
-    }
-  };
-
-  template <typename TRecipes, typename TLoggers, typename TCheckers> struct InstructionDetails {
-    TRecipes recipes{};
-    TLoggers loggers{};
-    TCheckers checkers{};
-
-    struct Package {
-      DeviceAggregate<TRecipes> recipes{};
-      DeviceAggregate<TLoggers> loggers{};
-      DeviceAggregate<TCheckers> checkers{};
-    };
-
-    auto sendTo([[maybe_unused]] auto const& device, auto& queue) {
-      auto r = recipes.get(queue, device);
-      auto l = loggers.get(queue, device);
-      auto c = checkers.get(queue, device);
-      return Package{r, l, c};
     }
   };
 
